10e5d32791a4264bc4e2129c978fd7f0b7e0447e
skiltion <taeho1517@gmail.com>
2025-03-20 20:45:27 +0900

    add databace endpoint

--- index.js
+++ index.js
@@ -15,14 +15,37 @@ app.listen(PORT, () => {
   });
 
 app.post('/articles', (req, res)=>{
-    let {title, content} = req.body
+  let {title, content} = req.body
     
-    db.run(`INSERT INTO articles (title, content) VALUES (?, ?)`,
-        [title, content],
-        function(err) {
-          if (err) {
-            return res.status(500).json({error: err.message});
-          }
-          res.json({id: this.lastID, title, content});
-        });
+  db.run(`INSERT INTO articles (title, content) VALUES (?, ?)`,
+    [title, content],
+      function(err) {
+        if (err) {
+          return res.status(500).json({error: err.message});
+        }
+        res.json({id: this.lastID, title, content});
+       });
+  });
+
+app.get('/articles', (req, res) => {
+    db.all(`SELECT * FROM articles`, [], (err, rows) => {
+      if (err) {
+        return res.status(500).json({error: err.message});
+      }
+      res.json(rows);
     });
+  });
+  
+app.get('/articles/:id', (req, res) => {
+  const id = req.params.id;
+  
+  db.get(`SELECT * FROM articles WHERE id = ?`, [id], (err, row) =>{
+    if(err) {
+      return res.status(500).json({error: err.message});
+    }
+    if(!row){
+      return res.status(404).json({error: '데이터가 없습니다.'});
+    }
+    res.json(row)
+  })
+})
